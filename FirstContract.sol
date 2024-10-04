de5a7a3be9eb6cadb53221df0333cf23cf20d616
Shiva Shashank <shashanktech99@gmail.com>
2024-10-04 16:42:49 -0500

    Create FirstContract.sol
    My first solidity program

---
+++ FirstContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyFirstContract {
+    bool isMale = true;
+    uint256 age = 24;
+    int256 favNumber = -3;
+    string name = "codepie";
+    address Address1 = 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF;
+    bytes32 someRandom = "Eth is fun";
+}
